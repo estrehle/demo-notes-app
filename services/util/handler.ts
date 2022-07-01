8764773743c6d3731a27f7be93c89cb5801cc47e
Elias Strehle <elias@circulartree.com>
2022-07-01 14:33:02 +0200

    enable cors

--- services/util/handler.ts
+++ services/util/handler.ts
@@ -20,6 +20,10 @@ export const handler = (
     return {
       statusCode,
       body: JSON.stringify(body),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
     };
   };
 };
--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,7 +1,16 @@
 import { Bucket, StackContext, Table } from '@serverless-stack/resources';
 
 export function StorageStack({ stack }: StackContext) {
-  const bucket = new Bucket(stack, 'Uploads');
+  const bucket = new Bucket(stack, 'Uploads', {
+    cors: [
+      {
+        allowedHeaders: ['*'],
+        allowedMethods: ['DELETE', 'GET', 'HEAD', 'POST', 'PUT'],
+        allowedOrigins: ['*'],
+        maxAge: '1 day',
+      },
+    ],
+  });
 
   const table = new Table(stack, 'Notes', {
     fields: {
